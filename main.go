0eafd5c60d39eb6c9c1ece7b5ddcddc7286f0e6a
zehan12 <zehan9211@gmail.com>
2024-06-08 01:42:35 +0530

    add index route

--- main.go
+++ main.go
@@ -14,5 +14,9 @@ func main() {
 
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"message": "server is running!"})
+	})
+
 	log.Fatal(app.Listen(PORT))
 }
