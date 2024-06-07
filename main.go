dc3fb5c8b0121599531a9df7e9d8257ea086ce5d
zehan12 <zehan9211@gmail.com>
2024-06-08 01:49:51 +0530

    add todo routing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/todo/react-go-todo-app/api"
 )
 
 func main() {
@@ -14,9 +15,20 @@ func main() {
 
 	app := fiber.New()
 
+	// group routes to v1
+	apiv1 := app.Group("/api/v1")
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"message": "server is running!"})
 	})
+
+	apiv1.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(map[string]string{"message": "Welcome to Todo Api!"})
+	})
+
+	// todo routes
+	todoRoutes := apiv1.Group("/todo")
+	todoRoutes.Post("/", api.HandleCreateTodo)
 
 	log.Fatal(app.Listen(PORT))
 }
